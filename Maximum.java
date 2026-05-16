8f83cbdeab58aca67e5965af04ff0e22fa9ad861
Sarvajna Rekhapalli111 <sarvajnarekhapalli111@gmail.com>
2026-05-16 20:55:03 +0530

    Added Maximum Product Solution

---
+++ Maximum.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+public class Maximum {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int case1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+        int case2 = nums[0] * nums[1] * nums[n - 1];
+
+        return Math.max(case1, case2);
+    }
+    public static void main(String[] args) {
+        Maximum s = new Maximum();
+        int[] nums = {1, 2, 3, 4};
+        int result = s.maximumProduct(nums);
+       System.out.println(result);
+    }
+}
